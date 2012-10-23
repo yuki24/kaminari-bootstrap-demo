f993c420cb0068c7023e377e7e2423df773e37bc
Yuki Nishijima <mail@yukinishijima.net>
2012-10-23 22:10:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Example of Kaminari Intergarion with Twitter Bootstrap. [see it in action](http://kaminari-bootstrap-demo.herokuapp.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaminariBootstrapDemo::Application.config.secret_token = '6f8d05370b74d5242d0c2e0aae1bfffaa897fc0dfa83f94472235da2e64be1e0c36f5849a984c3905ea409f60adcb5ee65a8439737f1c4c1255098fe8cda5bf1'
